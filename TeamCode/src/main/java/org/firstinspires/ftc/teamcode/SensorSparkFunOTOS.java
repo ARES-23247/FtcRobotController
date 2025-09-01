21a5c926a6de5ba351deef0806560992f83691e6
Chickinmuggins <jules.huss@gmail.com>
2025-09-01 09:34:46 -0400

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorSparkFunOTOS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorSparkFunOTOS.java
@@ -3,7 +3,7 @@
 
     Copyright (c) 2024 SparkFun Electronics
 */
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -25,7 +25,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * See the sensor's product page: https://www.sparkfun.com/products/24904
  */
 @TeleOp(name = "Sensor: SparkFun OTOS", group = "Sensor")
-@Disabled
+//@Disabled
 public class SensorSparkFunOTOS extends LinearOpMode {
     // Create an instance of the sensor
     SparkFunOTOS myOtos;
